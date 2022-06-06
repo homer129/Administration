4354ea46fcd438ff507512c5c1b950d4e7b76af6
Svyat <sv.romanovsky@ya.ru>
2022-06-06 23:46:04 +0300

    first commit

---
+++ src/AdminUser.java
@@ -0,0 +1,15 @@
+public class AdminUser {
+    public String userName;
+    public String password;
+    public String email;
+    public Boolean isLogedIn;
+
+    public AdminUser(String userName, String password, String email, Boolean isLogedIn) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.isLogedIn = isLogedIn;
+    }
+    
+    //public SimpleUser[] getMyUsers
+}
---
+++ src/SimpleUser.java
@@ -0,0 +1,12 @@
+public class SimpleUser extends AdminUser {
+    public Integer accountNumber;
+    public Boolean isPrime;
+    //
+
+    public SimpleUser(Integer accountNumber, Boolean isPrime, //) {
+        this.accountNumber = accountNumber;
+        this.isPrime = isPrime;
+        //
+        
+    }
+}
