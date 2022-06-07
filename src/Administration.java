efe55e0df414972af73c7e2581b67068834f6095
Svyat <sv.romanovsky@ya.ru>
2022-06-07 23:25:42 +0300

    commit2

--- src/Administration.java
+++ src/Administration.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Administration {
-    //   
+    public static void main(String[] args){
+        Scanner console1 = new Scanner(System.in);
+        System.out.println("Введите userName");
+        String userName1 = console1.nextLine(); 
+        System.out.println("Вы ввели " + userName1);
+    }
+      
 }
