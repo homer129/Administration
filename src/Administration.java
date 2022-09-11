170a97ba83fad9742249e926c5a69b04924a6d90
Svyat <sv.romanovsky@ya.ru>
2022-09-11 22:09:02 +0300

    commit8

--- src/Administration.java
+++ src/Administration.java
@@ -42,6 +42,13 @@ public class Administration {
                         SimpleUser simpleUser = new SimpleUser(username, password, email, false, SimpleUser.RandomNumber(101), false, allAdminUser.get(userNamePersonalAdmin));
                         allSimpleUser.put(username, simpleUser);
                     }                    
+                } else {
+                    if (allSimpleUser.containsKey(username)) {
+                        System.out.println("Login is used");
+                    } else {
+                        SimpleUser simpleUser = new SimpleUser(username, password, email, false, SimpleUser.RandomNumber(101), false, null);
+                        allSimpleUser.put(username, simpleUser);
+                    } 
                 }
             }                
         } if (Enter.equals("login")) {
@@ -74,29 +81,67 @@ public class Administration {
     }
     
     public static String loginLogout (String enter) {
-        Scanner console7 = new Scanner(System.in);
-        System.out.println("enter userName");
-        String enterUserName = console7.nextLine();
-        Scanner console8 = new Scanner(System.in);
-        System.out.println("enter password");
-        String enterPassword = console8.nextLine();
-        ArrayList<String> keys = new ArrayList<>(allAdminUser.keySet());
+        ArrayList<AdminUser> values = new ArrayList<>(allAdminUser.values());
+        ArrayList<SimpleUser> values1 = new ArrayList<>(allSimpleUser.values());    
         if (enter.equals("login")) {
-            for (int o = 0; o < keys.size(); o++) {
-                if (allAdminUser.get(keys.get(o)).userName.equals(enterUserName) && allAdminUser.get(keys.get(o)).password.equals(enterPassword)) {
-                    allAdminUser.get(keys.get(o)).isLogedIn = true;
-                    System.out.println(enterUserName + " is loged in.");
-                } else {
-                    allAdminUser.get(keys.get(o)).isLogedIn = false;
+            Scanner console7 = new Scanner(System.in);
+            System.out.println("enter userName");
+            String enterUserName = console7.nextLine();
+            Scanner console8 = new Scanner(System.in);
+            System.out.println("enter password");
+            String enterPassword = console8.nextLine();
+            if (allAdminUser.containsKey(enterUserName) == true) {
+                Administration.loginAdmin(values, values1, enterUserName, enterPassword);    
+            } if (allSimpleUser.containsKey(enterUserName) == true) {
+                Administration.loginUser(values1, values, enterUserName, enterPassword);                
+            }    
+        } if (enter.equals("logout")) {
+            for (int y = 0; y < values.size(); y++) {
+                if (values.get(y).isLogedIn == true) {
+                    values.get(y).isLogedIn = false;
+                    System.out.println(values.get(y).userName + " is loged out.");
                 }
             }
-        } if (enter.equals("logout")) {
-            allAdminUser.get(enterUserName).isLogedIn = false;
-            System.out.println(enterUserName + " is loged out.");
+            for (int z = 0; z < values1.size(); z++) {
+                if (values1.get(z).isLogedIn == true) {
+                    values1.get(z).isLogedIn = false;
+                    System.out.println(values1.get(z).userName + " is loged out.");
+                }
+            }
         }
         return null;
     }
+
+    public static void loginAdmin (ArrayList<AdminUser> list, ArrayList<SimpleUser> list1, String username, String pasword) {
+        for (int r = 0; r < list.size(); r++) {
+            if (list.get(r).userName.equals(username) && list.get(r).password.equals(pasword)) {
+                list.get(r).isLogedIn = true;
+                System.out.println(list.get(r).userName + " is loged in.");
+            } else {
+                list.get(r).isLogedIn = false;
+            }
+        }
+        for (int t = 0; t < list1.size(); t++) {
+            list1.get(t).isLogedIn = false;
+        }
+    }
+
+    public static void loginUser (ArrayList<SimpleUser> list2, ArrayList<AdminUser> list3, String username1, String pasword1) {
+        for (int o = 0; o < list2.size(); o++) {
+            if (list2.get(o).userName.equals(username1) && list2.get(o).password.equals(pasword1)) {
+                list2.get(o).isLogedIn = true;
+                System.out.println(list2.get(o).userName + " is loged in.");
+            } else {
+                list2.get(o).isLogedIn = false;
+            }
+        }
+        for (int l = 0; l < list3.size(); l++) {
+            list3.get(l).isLogedIn = false;
+        }
+    }
 }
+
+
 
 
 
